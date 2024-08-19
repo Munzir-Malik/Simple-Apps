3441114812a20e22ead53f7211911eaede3962a4
Munzir <>
2024-08-19 15:34:10 +0200

    Add new features and enhancements to app

--- ATM/ATM.java
+++ ATM/ATM.java
@@ -1,49 +1,95 @@
 /*********************      This is ATM simultion program       *********************/
-//In this app i tried to applicat most of the basics of Java language.
-// You will find the details in the README file.
+/*In this app i tried to applicat most of the basics of Java language,
+  and I will improve this program while I am learning new stuff in the language to practice...*/
+// You will find the details and enhancements in the README file.
 // Enioy :
 
 import java.util.Scanner;
 
 public class ATM {
-  public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-    int balance = 1000, amount, choice;
+  private int balance = 1000;
+  private int transactionLimit = 1000;
 
-    System.out.println("Welcome to \'Munzir\' ATM... \n");
+  //Method to view balance
+  public void getBalance(){
+    System.out.println("Your curent balance is: " + this.balance);
+  }
+
+  //Method to deposite money
+  public void depositeMoney(int amount){
+    if (amount > this.transactionLimit) {
+      System.out.println("Transaction limit exceeded!");
+    }
+    else if(amount > 0){
+      this.balance = this.balance + amount;
+      System.out.println("Your curent balance is: " + this.balance);
+    }
+    else System.out.println("Invalid amount.");
+
+  }
+  //Method to withdraw money
+  public void withdrawMoney(int amount){
+    if (amount > this.transactionLimit) {
+      System.out.println("Transaction limit exceeded!");
+    }
+    else if (amount < 0){
+      System.out.println("Invalid amount.");
+    }
+    else if(amount > this.balance) {
+      System.out.println("You have not enough balance.");
+      System.out.println( "\"Check your balance or depsite some money and try again.\"");
+    }
+    else {
+        this.balance = this.balance - amount;
+        System.out.println("Your curent balance is: " + this.balance);
+    }
+  }
+
+  //Method to clear screen
+  public void cls(){
+    for(int i = 1; i <= 50; i++){
+      System.out.println();
+    }
+  }
+
+  public static void main(String[] args) {
+    int choice, amount;
+
+
+    Scanner input = new Scanner(System.in);
+
+    ATM atm = new ATM();
+
+    System.out.println("Welcome to \'Munzir\' ATM... ");
 
     while (true) {
-      System.out.println("1. Check Balance\n" + "2. Deposit Money\n" + "3. Withdraw Money\n" + "4. Exit");
+      System.out.println("\n1. Check Balance\n" + "2. Deposit Money\n" + "3. Withdraw Money\n" + "4. Exit");
+      System.out.println("Note: The maximum amount you can withdraw or depsoite in each process is: 1000");
       System.out.print("Please choose an option: ");
 
       choice = input.nextInt();
       switch (choice) {
         case 1: 
-          System.out.println("Your current balance is: " +balance);
+          atm.cls();
+          atm.getBalance();
           break;
       
         case 2: 
           System.out.println("Enter the amount to deposit: ");
           amount = input.nextInt();
-          balance = balance + amount;
-          System.out.println("Your curent balance is: " + balance);
+          atm.cls();
+          atm.depositeMoney(amount);
           break;
       
         case 3: 
           System.out.println("Enter the amount to withdraw: ");
           amount = input.nextInt();
-          if(amount > balance) {
-            System.out.println("You have not enough balance.");
-            System.out.println( "\"Check your balance or depsite some money and try again.\"");
-            continue;
-          }
-          else {
-            balance = balance - amount;
-            System.out.println("Your curent balance is: " + balance);
-          }
+          atm.cls();
+          atm.withdrawMoney(amount);
           break;
       
         case 4: 
+        atm.cls();
           System.out.println("Thank you for using \'Munzir\' ATM. Goodbye!");
           input.close();
           return;
